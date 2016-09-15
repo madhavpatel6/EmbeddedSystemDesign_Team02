3e09433a04db10c83586b0a572ece70285d4a802
skralick <skralick@vt.edu>
2016-09-15 14:35:06 -0400

    changed basic server files a little bit so we have an echo file now and prelim routes from 2000 to 2001

--- PythonServer/echoServer.py
+++ PythonServer/echoServer.py
@@ -6,18 +6,13 @@ sock2 = socket.socket()
 
 # allow ports to be reused, helpful when programs crash and restart
 sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1);
-sock2.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1);
 
 sock.bind(("", 2000))
-sock2.bind(("", 2001))
 print("bound")
 sock.listen(5)
-sock2.listen(5)
 print("listen")
 client_sock, address = sock.accept()
 print("conn0 complete")
-client2_sock, addy2 = sock2.accept()
-print("conn1 complete")
 while(True):
     try:
         data = client_sock.recv(512)
@@ -27,10 +22,10 @@ while(True):
         print("attempting to reconnect 2000");
     if(data):
         try:
-            client2_sock.send(data)
+            client_sock.send(data)
         except:
-            client2_sock, addy2 = sock2.accept()
-            print("attempting to reconnect 2001");
+            client_sock, address = sock.accept()
+            print("attempting to reconnect 2000");
     else:
         break
 
