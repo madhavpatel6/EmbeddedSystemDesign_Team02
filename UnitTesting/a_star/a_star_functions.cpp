319f15c25ec8775cd61e3b327dbd8b8edf7fe61e
skralick <skralick@vt.edu>
2016-10-24 15:05:08 -0400

    fixes and refactored a ton if things in a*, also enabled far searches by extending runtime of alg so some take a very long time (1.8s)

--- UnitTesting/a_star/a_star_functions.cpp
+++ UnitTesting/a_star/a_star_functions.cpp
@@ -4,7 +4,8 @@
 #include "iostream"
 using namespace std;
 
-#define listLength 100
+#define listLength 5000
+#define MAXLOOPCOUNT 1100
 #define maxVerticies 25
 #define pointsToObject 4
 
@@ -63,61 +64,75 @@ void addVertex(Point one){
 	vertexLen++;
 }
 
-/* ray starts at -inf -inf and goes to rayend, intersects with 3 4*/
-bool isIntersectingRay(Point rayEnd, Point three, Point four, bool *isVertex){
+bool isIntersecting(Point rayStart, Point rayEnd, Point three, Point four, bool *isVertex){
 	*isVertex = false;
-	Point rayStart = {.x = INT_MIN, .y = INT_MIN};
 
-	// cout << rayEnd.x << " " << rayEnd.y << "\n";
+	//two vertical special case
+	if(rayStart.x == rayEnd.x && three.x == four.x && rayEnd.x == three.x){
+		//vertical coincident lines
 
-	// cout << three.x << " " << three.y << "\n";
-	// cout << four.x << " " << four.y << "\n";
-
-	//m1 is only inf if one is -inf y
-	float m1 = ((float) rayEnd.y - (float)rayStart.y) / ((float)rayEnd.x - (float)rayStart.x);
-	float b1 = rayEnd.y - m1 * rayEnd.x;
-
-	float m2, b2, x, y;
-	if(four.x - three.x == 0 ){
-		x = four.x;
-		y = m1 * x + b1;
-		// cout << "vertical line" << endl;
-	}else{
-		m2 = (four.y - three.y) / (four.x - three.x);
-	    b2 = three.y - m2 * three.x;
-	    // cout << "m1 " << m1 << " b1 " << b1 << endl;
-	    // cout << "m2 " << m2 << " b2 " << b2 << endl;
-	    // cout << "b2-b1 " << b2-b1 << " m1-m2 " << m1-m2 << endl;
-	    x = (b2 - b1) / (m1 - m2);
-		y = m1 * x + b1;
+		//if ray starts or ends at the edges of y they are a vertex and will be double counted later
+		*isVertex = (rayEnd.y   == min(three.y, four.y) || rayEnd.y   == max(three.y, four.y) ||
+					 rayStart.y == min(three.y, four.y) || rayStart.y == max(three.y, four.y));
+		return rayEnd.y   >= min(three.y, four.y) && rayEnd.y   <= max(three.y, four.y) ||
+			   rayStart.y >= min(three.y, four.y) && rayStart.y <= max(three.y, four.y);
+	}else if(rayStart.x == rayEnd.x && three.x == four.x){
+		// vertical non coincident lines
+		*isVertex = false;
+		return false;
 	}
 
+	float m1 = (rayEnd.y - rayStart.y) / (rayEnd.x - rayStart.x);
+	float m2 = (four.y   - three.y   ) / (four.x   - three.x   );
 
-	// cout << "m1 " << m1 << " b1 " << b1 << endl;
-	// cout << "m2 " << m2 << " b2 " << b2 << endl;
+	float b1 = rayEnd.y - m1 * rayEnd.x;
+    float b2 = three.y  - m2 * three.x;
 
-	// cout << "x " << x << " y " << y << endl;
+    // parallel lines special case
+    if(m1 == m2 && b1 == b2){
+    	*isVertex = (rayEnd.y   == min(three.y, four.y) || rayEnd.y   == max(three.y, four.y) ||
+					 rayStart.y == min(three.y, four.y) || rayStart.y == max(three.y, four.y)) &&
+    				(rayEnd.x   == min(three.x, four.x) || rayEnd.x   == max(three.x, four.x) ||
+					 rayStart.x == min(three.x, four.x) || rayStart.x == max(three.x, four.x));
+    	return (rayEnd.y   >= min(three.y, four.y) && rayEnd.y   <= max(three.y, four.y) ||
+			    rayStart.y >= min(three.y, four.y) && rayStart.y <= max(three.y, four.y)) && 
+    		   (rayEnd.x   >= min(three.x, four.x) && rayEnd.x   <= max(three.x, four.x) ||
+			    rayStart.x >= min(three.x, four.x) && rayStart.x <= max(three.x, four.x));
+    }else if ((m1 == m2)){
+    	*isVertex = false;
+    	return false;
+    }
+    float x, y;
 
-	//return true if that intersecting point is in both of the line segments
-	if ( x <= rayEnd.x && y <= rayEnd.y &&
+    // find the intersection point
+    if( rayEnd.x == rayStart.x){
+    	x = rayEnd.x;
+    	y = m2 * x + b2;
+    }else if(three.x == four.x){
+    	x = three.x;
+    	y = m1 * x + b1;
+    }else{
+    	x = (b2 - b1) / (m1 - m2);
+		y = m1 * x + b1;
+    }
+
+
+	*isVertex = ( (x == min(three.x, four.x) ||
+				   x == max(three.x, four.x)     ) &&
+				  (y == min(three.y, four.y)  ||
+				   y == max(three.y, four.y)     )  );
+
+	return  x >= min(rayStart.x, rayEnd.x) &&
+			x <= max(rayStart.x, rayEnd.x) &&
+			y >= min(rayStart.y, rayEnd.y) &&
+			y <= max(rayStart.y, rayEnd.y) &&
+
 			x >= min(three.x, four.x) &&
 			x <= max(three.x, four.x) &&
 			y >= min(three.y, four.y) &&
-			y <= max(three.y, four.y) ){
-
-		if ( (x == min(three.x, four.x) ||
-				x == max(three.x, four.x)) &&
-				(y == min(three.y, four.y)||
-				y == max(three.y, four.y) ) ){
-			*isVertex = true;
-					
-		}
-
-		return true;
-	}else{
-		return false;
-	}
+			y <= max(three.y, four.y);
 }
+
 
 /* determines if a point is in the arena */
 bool isInArena(Point one){
@@ -126,6 +141,12 @@ bool isInArena(Point one){
 	int i;
 
 	bool crap;
+	bool crap2;
+	int count2 = 0; 
+	int vertCount2 = 0;
+
+	Point s1 = {.x = INT_MIN, .y = INT_MIN};
+	Point s2 = {.x = INT_MIN, .y = one.y};
 
 	//return true if point is a vertex
 	for(i = 0; i < vertexLen; i++){
@@ -139,7 +160,7 @@ bool isInArena(Point one){
 		Point end = (i == vertexLen-1 ? verticies[0] : verticies[i+1]);
 		// cout << "start " << start.x << " " << start.y << endl;
 		// cout << "end " << end.x << " " << end.y << endl;
-		if( isIntersectingRay(one, start, end, &crap)){
+		if( isIntersecting(s1, one, start, end, &crap)){
 			if(crap == true){
 				vertCount++;
 			}else{
@@ -147,9 +168,18 @@ bool isInArena(Point one){
 				count++;
 			}
 		}
+		if( isIntersecting(s2, one, start, end, &crap2)){
+			// cout << crap2<< endl;
+			if(crap2 == true){
+				vertCount2++;
+			}else{
+				// cout << "end " << end.x << " " << end.y << endl;
+				count2++;
+			}
+		}
 	}
-	// cout << count << " " << vertCount << endl;
-	if((count + (vertCount / 2)) % 2 == 0){
+	// cout << count << " " << vertCount << " " << count2 << " " << vertCount2 << endl;
+	if((count + (vertCount / 2)) % 2 == 0 || (count2 + (vertCount2 / 2)) % 2 == 0){
 		return false;
 	}else{
 		return true;
@@ -162,7 +192,12 @@ bool isInObstacle(Point one, int obIndex){
 	int i;
 
 	bool crap;
+	bool crap2;
+	int count2 = 0; 
+	int vertCount2 = 0;
 
+	Point s1 = {.x = INT_MIN, .y = INT_MIN};
+	Point s2 = {.x = INT_MIN, .y = one.y};
 
 	//return true if point is a vertex
 	for(i = 0; i < pointsToObject; i++){
@@ -174,7 +209,7 @@ bool isInObstacle(Point one, int obIndex){
 	for(i = 0; i < pointsToObject; i++){
 		Point start = obstacles[obIndex][i];
 		Point end = i == pointsToObject-1 ? obstacles[obIndex][0] : obstacles[obIndex][i+1];
-		if( isIntersectingRay(one, start, end, &crap)){
+		if( isIntersecting(s1, one, start, end, &crap)){
 			if(crap == true){
 				vertCount++;
 			}else{
@@ -182,10 +217,20 @@ bool isInObstacle(Point one, int obIndex){
 				count++;
 			}
 		}
+		if( isIntersecting(s2, one, start, end, &crap2)){
+			if(crap2 == true){
+				vertCount2++;
+			}else{
+				// cout << "end " << end.x << " " << end.y << endl;
+				count2++;
+			}
+		}
+
 	}
 
-	// cout << "coutnt " <<count<< endl;
-	if((count + (vertCount / 2)) % 2 == 0){
+	// cout << "count  " <<count<< " " << vertCount<< endl;
+	// cout << "count2 " <<count2<< " " << vertCount2<< endl;
+	if((count + (vertCount / 2)) % 2 == 0 || (count2 + (vertCount2 / 2)) % 2 == 0){
 		return false;
 	}else{
 		return true;
@@ -198,6 +243,12 @@ bool isInTarget(Point one, int obIndex){
 	int i;
 
 	bool crap;
+	bool crap2;
+	int count2 = 0; 
+	int vertCount2 = 0;
+
+	Point s1 = {.x = INT_MIN, .y = INT_MIN};
+	Point s2 = {.x = INT_MIN, .y = one.y};
 
 
 	//return true if point is a vertex
@@ -210,18 +261,27 @@ bool isInTarget(Point one, int obIndex){
 	for(i = 0; i < pointsToObject; i++){
 		Point start = targets[obIndex][i];
 		Point end = i == pointsToObject-1 ? targets[obIndex][0] : targets[obIndex][i+1];
-		if( isIntersectingRay(one, start, end, &crap)){
+		if( isIntersecting(s1, one, start, end, &crap)){
 			if(crap == true){
 				vertCount++;
 			}else{
 				// cout << "end " << end.x << " " << end.y << endl;
 				count++;
 			}
+
+		}
+		if( isIntersecting(s2, one, start, end, &crap2)){
+			if(crap2 == true){
+				vertCount2++;
+			}else{
+				// cout << "end " << end.x << " " << end.y << endl;
+				count2++;
+			}
 		}
 	}
 
 	// cout << "coutnt " <<count<< endl;
-	if((count + (vertCount / 2)) % 2 == 0){
+	if((count + (vertCount / 2)) % 2 == 0 || (count2 + (vertCount2 / 2)) % 2 == 0){
 		return false;
 	}else{
 		return true;
@@ -230,7 +290,7 @@ bool isInTarget(Point one, int obIndex){
 bool isOutAllTargets(Point one){
 	int i;
 	for(i = 0; i < targetLen; i++){
-		if(isInTarget(one, i)){
+		if(isInTarget(one, i) == true){
 			return false;
 		}
 	}
@@ -240,6 +300,7 @@ bool isOutAllObstacles(Point one){
 	int i;
 	for(i = 0; i < obstacleLen; i++){
 		if(isInObstacle(one, i) == true){
+			// cout << "in ob" << i<<endl;
 			return false;
 		}
 	}
@@ -248,6 +309,7 @@ bool isOutAllObstacles(Point one){
 
 /* determines if a point is traversable */
 bool isEmpty(Point one){
+	// cout << isOutAllObstacles(one) << isOutAllTargets(one) << isInArena(one)<<endl;
 	return isOutAllObstacles(one) && isOutAllTargets(one) && isInArena(one);
 }
 
@@ -280,7 +342,7 @@ bool getAdjacent(Point target, Point *one, Point *two, Point *three, Point *four
 
 /* estimates distance to destination by counting city blocks */
 int calculateH(Point start, Point stop){
-	return abs(start.x - stop.x) + abs(start.y - stop.y);
+	return fabs(start.x - stop.x) + fabs(start.y - stop.y);
 }
 
 bool addToClosedList(Point one, Point end){
@@ -328,16 +390,34 @@ bool isInClosedList(Point one){
 	return false;
 }
 
-bool addWalkableLocations(){
-	return false;
-}
+void addWalkableLocations(Point current, Point stop, int parentIndex){
+	Point one, two, three, four;
+	getAdjacent(current, &one, &two, &three, &four, parentIndex); // determines f scores
 
-Point findBestStep(){
-	Point ex = {.x = 0, .y = 0};
-	return ex;
+	if(isEmpty(one) && !isInOpenList(one) ){//&& !isInClosedList(one)){
+		addToOpenList(one, stop);
+	}
+	if(isEmpty(two)&& !isInOpenList(two) ){//&& !isInClosedList(one)){
+		addToOpenList(two, stop);
+	}
+	if(isEmpty(three)&& !isInOpenList(three) ){//&& !isInClosedList(one)){
+		addToOpenList(three, stop);
+	}
+	if(isEmpty(four)&& !isInOpenList(four)){
+		addToOpenList(four, stop);
+	}
 }
 
 bool finalizePath(){
+	int i;
+	int index = 0;
+	for(i = closedLen-1; i > 0; i = closedList[i].parent){
+		//cout << i<<" patStep "<< closedList[i].x << " " << closedList[i].y <<" " << closedList[i].parent<<endl;
+		finalPath[index] = closedList[i];
+		index++;
+	}
+	finalPath[index] = closedList[i];
+	finalLen = index +1;
 	return false;
 }
 
@@ -354,64 +434,67 @@ bool findPath(Point start, Point stop){
 	int parentIndex = 0;
 
 	// while(destination is not in closed list)
-	while( ! isInClosedList(stop) && count <= 100){
-		// cout << current.x << " " << current.y << " "<< current.gScore <<" " << current.hScore<<endl;
-		// add walkable locations to open list (calculating F = G + H scores for them)
-		Point one, two, three, four;
-		getAdjacent(current, &one, &two, &three, &four, parentIndex); // determines f scores
+	while( !isInClosedList(stop) && count <= MAXLOOPCOUNT){
+		// possible faster while condition (current.x != stop.x || current.y != stop.y) && count <= MAXLOOPCOUNT){
 
-		if(isEmpty(one) && !isInOpenList(one) ){//&& !isInClosedList(one)){
-			addToOpenList(one, stop);
-		}
-		if(isEmpty(two)&& !isInOpenList(two) ){//&& !isInClosedList(one)){
-			addToOpenList(two, stop);
-		}
-		if(isEmpty(three)&& !isInOpenList(three) ){//&& !isInClosedList(one)){
-			addToOpenList(three, stop);
-		}
-		if(isEmpty(four)&& !isInOpenList(four)){
-			addToOpenList(four, stop);
-		}
-		// cout << "openLen " << openLen << endl;
+		// add walkable locations to open list (calculating F = G + H scores for them)
+		addWalkableLocations(current, stop, parentIndex);
 
 		int minFscore = INT_MAX; // i hope openList has something in it
 		int minFScoreIndex = -1;
 		for(int j = 0; j < openLen; j++){
 			if( !isInClosedList(openList[j]) && 
-				openList[j].gScore + openList[j].hScore < minFscore){
+					openList[j].gScore + openList[j].hScore < minFscore){
 				minFscore = openList[j].gScore + openList[j].hScore;
 				minFScoreIndex = j;
 			}
 
 		}
-		// cout << "chosen OpenIndex " << minFScoreIndex<< endl;
-
-		// add lowest F score to the closed list
-		// openList[minFScoreIndex].parent = parentIndex;
+		// cout << openLen << " " << count << " " << closedLen<< endl;
 		parentIndex = closedLen;
 		addToClosedList(openList[minFScoreIndex], stop);
 		current = openList[minFScoreIndex];
-		// parentIndex = minFScoreIndex;
+
 		count ++;
 	}
 
-
-	// for( int i =0; i < closedLen; i++){
-	// 	cout << i<<" patStep "<< closedList[i].x << " " << closedList[i].y <<" " << closedList[i].parent<<endl;
-	// }
-	int i, index;
-	for(i = closedLen-1; i > 0; i = closedList[i].parent){
-		//cout << i<<" patStep "<< closedList[i].x << " " << closedList[i].y <<" " << closedList[i].parent<<endl;
-		finalPath[index] = closedList[i];
-		index++;
-	}
-	finalPath[index] = closedList[i];
-	finalLen = index +1;
-	return count < 100;
+	finalizePath();
+	return count < MAXLOOPCOUNT;
 }
 
 void printPath(){
 	for( int i =0; i < finalLen; i++){
 		cout << i<<" patStep "<< finalPath[i].x << " " << finalPath[i].y <<" " << finalPath[i].parent<<endl;
+	}
+}
+
+void resetPath(){
+	// for(int i = 0; i < openLen; i++){
+
+	// }
+	// for(int i = 0; i < closedLen; i++){
+		
+	// }
+	// for(int i = 0; i < finalLen; i++){
+		
+	// }
+
+	openLen = 0;
+	closedLen = 0;
+	finalLen = 0;
+}
+
+void printWorld(){
+	for(int i = 0 ; i < targetLen; i++){
+		for(int j = 0; j < pointsToObject; j++){
+			cout << "("<<targets[i][j].x << "," << targets[i][j].y << ") " ;
+		}
+		cout <<endl;
+	}
+	for(int i = 0 ; i < obstacleLen; i++){
+		for(int j = 0; j < pointsToObject; j++){
+			cout << "("<<obstacles[i][j].x << "," << obstacles[i][j].y << ") " ;
+		}
+		cout <<endl;
 	}
 }
